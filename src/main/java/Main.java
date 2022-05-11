e156c575d03f750547ccf58958c4f4c614a73b5e
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-11 12:45:17 -0400

    otro commit mas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.google.cz");
+		driver.get("https://www.google.com");
 		
 		driver.quit();
 	}
